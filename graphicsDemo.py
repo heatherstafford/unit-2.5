c2327bfeba1adffa958cfcd897a0db778528967f
heatherstafford <35492769+heatherstafford@users.noreply.github.com>
2018-02-08 12:07:40 -0500

    Update graphicsDemo.py

--- graphicsDemo.py
+++ graphicsDemo.py
@@ -16,11 +16,13 @@ blueCircle = CircleAsset(50,blackOutline,blue) #radius, outline, fill
 greenEllipse = EllipseAsset(100,50,blackOutline, green) #width, height, outline, fill
 blackLine = LineAsset(50,160,blackOutline) #x-endpoint, y-endpoint, lineStyle
 redTriangle = PolygonAsset([(0,0),(120,180),(60,300)],blackOutline, red) #enpoints, outlines, fill
+text = TextAsset('Heather', fill = red, style = 'italic 35pt Times') #text, other options
 
 Sprite(redRectangle)
 Sprite(blueCircle,(300,50)) #(moving it over by coordinates)
 Sprite(greenEllipse, (40,200))
 Sprite(blackLine, (40,100))
 Sprite(redTriangle)
+Sprite(text, (300,50))
 
 App().run()
