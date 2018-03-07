96a80400a06c3836b8f37bd4b04fedd0f52ec613
heatherstafford <35492769+heatherstafford@users.noreply.github.com>
2018-03-07 08:18:16 -0500

    Updated from Brython Server: 3/7/2018 8:18:15 AM

--- remainder.py
+++ remainder.py
@@ -1,1 +1,12 @@
+#Heather Stafford
+#3/7/18
+#remainder.py
 
+from random import randint
+
+num1 = randint(1,10)
+num2 = randint(1,10)
+
+answer = int(input('What is '+ str(num1) + '/' + str(num2) + '?'))
+if answer == num1%num2:
+    
