eed38f046cce2361aa5554bf5ee792b98f6e5b28
heatherstafford <35492769+heatherstafford@users.noreply.github.com>
2018-03-07 08:33:13 -0500

    Updated from Brython Server: 3/7/2018 8:33:12 AM

--- remainder.py
+++ remainder.py
@@ -9,12 +9,19 @@ num1 = randint(10,20)
 num2 = randint(1,10)
 
 yellow = Color(0xFFFF00, 1)
+black = Color(0x000000,1)
+
 yellowCircle = CircleAsset(100, LineStyle(2,black), yellow)
+blackCircle = CircleAsset(50, LineStyle(2,black),black)
+yellowEdit = CircleAsset(50, LineStyle(2,yellow), yellow)
 
 answer = float(input('What is the remainder of '+ str(num1) + '/' + str(num2) + '?'))
 if answer == num1%num2:
     Sprite(yellowCircle)
+    Sprite(blackCircle,(50,60))
+    Sprite(yellowEdit, (50,40))
 else:
     Sprite(yellowCircle)
+    Sprite(blackCircle, (50,100))
     
 App().run()
