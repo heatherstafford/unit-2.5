de96820e850993a33cec68fd154a0e2717c6d6f3
heatherstafford <35492769+heatherstafford@users.noreply.github.com>
2018-03-07 08:23:05 -0500

    Updated from Brython Server: 3/7/2018 8:23:04 AM

--- remainder.py
+++ remainder.py
@@ -3,10 +3,17 @@
 #remainder.py
 
 from random import randint
+from ggame import *
 
 num1 = randint(1,10)
 num2 = randint(1,10)
 
+yellow = Color(0xFFFF00)
+yellowCircle = CircleAsset(100, LineStyle(2,black), yellow)
+
 answer = int(input('What is '+ str(num1) + '/' + str(num2) + '?'))
 if answer == num1%num2:
+    sprite(yellowCircle)
+else:
+    sprite(yellowCirlce)
     
